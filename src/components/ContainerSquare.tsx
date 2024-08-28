55069c962187e3587903c8a803f3eebc7179ea5b
AndrosZantis <andros.zantis@gmail.com>
2024-08-28 22:20:24 +0100

    added comments

--- src/components/ContainerSquare.tsx
+++ src/components/ContainerSquare.tsx
@@ -3,18 +3,23 @@ import './ContainerSquare.css';
 
 
 const ContainerSquare: React.FC = () => {
+  // useState to track if the container is active or not
   const [isActive, setIsActive] = useState(false);
 
+  // Handle the toggle event and reset automatically after 2000ms
   useEffect( () => {
     if(isActive){
+      // Set as inactive after 2 seconds
       const timer = setTimeout(() => {
         setIsActive(false);
       }, 2000);
-
+      
+      // Clean up the timer when component is unmounted or when isActive changes
       return () => clearTimeout(timer);
     }
   }, [isActive]);
 
+  // Function to toggle the active/inactive state when the container is clicked
   const toggleActiveState = () => {
     setIsActive(!isActive);
   };
@@ -26,7 +31,7 @@ const ContainerSquare: React.FC = () => {
     >
       <div className={`smaller-square ${isActive? 'active' : 'inactive'}`}></div>
     </div>
-  )
+  );
 
 
 };
